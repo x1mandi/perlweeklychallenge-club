2acc0bd3400d382781a2c03a2064b37aaa248c13
Csaba Simandi <20855193+x1mandi@users.noreply.github.com>
2020-07-27 10:24:01 -0400

    Started to work on challenge 71 task 1, solution is incomplete

---
+++ challenge-071/x1mandi/perl/ch-1.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+use utf8;
+use 5.28.0;
+use Data::Dumper;
+
+#Solution for Taks 1 - Peak Element by x1mandi
+# Given is $N > 1. Generate array of size $N,
+# filled up with random unique integers between 1 and 50.
+# Return the peak elements from the array.
+# Example 1
+# Array: [ 18, 45, 38, 25, 10, 7, 21, 6, 28, 48 ]
+# Peak: [ 48, 45, 21 ]
+# Example 2
+# Array: [ 47, 11, 32, 8, 1, 9, 39, 14, 36, 23 ]
+# Peak: [ 47, 32, 39, 36 ]
+
+my $N = $ARGV[0];
+
+die "ERROR: input not defined!" unless defined $N;
+die "ERROR: $N is not greater than 1" unless ($N > 1);
+
+peak_elements();
+
+
+
+sub peak_elements {
+    #Filling up the array with random numbers 
+    my %randoms;
+    for (0..$N) {
+      $randoms{$_} = int rand(50);
+    }
+    
+
+ 
+                  
+    #Just to check the original array
+    print Dumper(keys %randoms);
+    
+}
+
+sub get_random_int {
+  my $num = int rand(51);
+  return $num;
+}
